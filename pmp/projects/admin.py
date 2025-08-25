416a0bb1d43106696da44267affd00270cde3a52
kvmofv <kvmofv@gmail.com>
2025-08-25 20:09:46 +0300

    Registered projects app's models into the admin panel

--- pmp/projects/admin.py
+++ pmp/projects/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from . models import Project, Plan, Space, PlanSpace, Equipment, Furniture
 
-# Register your models here.
+class PlanInline(admin.TabularInline):
+    model = Plan
+    extra = 1
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [PlanInline]
+    list_display = ("name", "type", "created_by", "creation_date", "finish_date", "status")
+    list_filter = ("type", "creation_date", "status")
+    search_fields = ("name", "type")
+
+class SpaceInline(admin.TabularInline):
+    model = PlanSpace
+    extra = 1
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    inlines = [SpaceInline]
+    list_display = ("reference_name", "description", "status", "project")
+    search_fields = ("reference_name",)
+
+@admin.register(Space)
+class SpaceAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "status")
+    search_fields = ("name",)
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "requirement")
+    search_fields = ("name",)
+
+@admin.register(Furniture)
+class FurnitureAdmin(admin.ModelAdmin):
+    list_display = ("name", "requirement")
+    search_fields = ("name",)
