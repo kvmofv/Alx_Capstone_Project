45475ba0d7dfa831edd0b5d0e451b16f3ed7f4db
kvmofv <kvmofv@gmail.com>
2025-08-30 22:08:11 +0300

    accounts views updated to match urls paths

--- pmp/accounts/views.py
+++ pmp/accounts/views.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from .serializers import CustomUserSerializer, LoginSerializer, UserUpdateSerializer
-from .permissions import IsAdmin
 
 User = get_user_model()
 
